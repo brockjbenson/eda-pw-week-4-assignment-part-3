6de14938b14ccd7a1c6557a7ce2659e622432866
Brock Benson <brockjbenson@outlook.com>
2022-12-04 15:27:51 -0600

    done non stretch

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,28 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+function addItem ( item ) {
+    basket.push(item);
+    return true;
+}
+console.log(`Basket is ${basket}`);
+console.log('Adding item Milk:', addItem('milk'));
+console.log('Adding item Eggs:', addItem('eggs'));
+console.log('Adding item Cheese:', addItem('cheese'));
+console.log('Adding item Bread:', addItem('bread'));
+console.log(`Basket is now ${basket}`);
+
+
+function listItems () {
+    for (let i = 0; i < basket.length; i++) {
+        console.log('Listing item:', basket[i]);
+      }
+}
+console.log(listItems());
+
+function empty () {
+    while(basket.length > 0) {
+        basket.pop();
+    }
+}
